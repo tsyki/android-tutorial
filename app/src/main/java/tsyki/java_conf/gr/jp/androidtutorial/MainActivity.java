717340db9cef38d6817548b177d7c79bdd445949
Toshiyuki Imaizumi <evening_glow@khf.biglobe.ne.jp>
2017-01-09 20:36:38 +0900

    条件分岐、繰り返しのサンプルを追加

--- app/src/main/java/tsyki/java_conf/gr/jp/androidtutorial/MainActivity.java
+++ app/src/main/java/tsyki/java_conf/gr/jp/androidtutorial/MainActivity.java
@@ -11,9 +11,68 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // XXX ここでoperationは+、-などの文字列にしたいが、equalsを使う必要があるので避ける
+        int operation = 0;
         int num1 = 2;
         int num2 = 3;
-        int calcResult = num1 + num2;
+        int calcResult;
+        if (operation == 0) {
+            calcResult = num1 + num2;
+        } else {
+            calcResult = num1 - num2;
+        }
+        System.out.println("if " + calcResult);
+
+        switch (operation) {
+            case 0:
+                calcResult = num1 + num2;
+                break;
+            case 1:
+                calcResult = num1 - num2;
+                break;
+            default:
+                calcResult = 0;
+                break;
+        }
+        System.out.println("switch " + calcResult);
+
+        // 10回繰り返し
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            // XXX ここは標準出力などにしたい
+            sum = sum + 1;
+        }
+        System.out.println("for " + sum);
+
+        // 1から10の合計
+        sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+        }
+        System.out.println("for2 " + sum);
+
+        // 逆バージョン
+        sum = 0;
+        for (int i = 10; i > 0; i--) {
+            sum += i;
+        }
+        System.out.println("for3 " + sum);
+
+        // conitnue
+        // 奇数のみ表示
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 1) {
+                System.out.println(i);
+            }
+        }
+        // while
+        sum = 0;
+        int i = 1;
+        while (i <= 10) {
+            sum += i;
+            i++;
+        }
+        System.out.println("while " + sum);
 
         // 四則演算サンプル
         // 引き算(減算)
